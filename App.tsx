430a82334ad9f180dbe15a1c22a278a903e732a2
rudans987 <rudans987@naver.com>
2022-12-18 20:37:46 +0900

    리덕스쓸 때 주의점

--- App.tsx
+++ App.tsx
@@ -7,6 +7,7 @@
 // TouchableWithoutFeedback, TouchableNativeFeedback
 // Tab.Screen 쓸때 특정 스크린 간에 공통 속성이 있을 때나 children이 하나여야 된다는 에러 날때 Tab.Group태그로 묶어야 한다.
 
+//  Provider 바깥에서 useSelector를 못쓰기 때문에 AppInner로 따로 빼서 거기서 useSelector를 쓴다.
 import * as React from 'react';
 import {Provider} from 'react-redux';
 import AppInner from './AppInner';
